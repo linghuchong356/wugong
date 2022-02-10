579966d7bfa22860c74843efbc6297a3f6de6891
linghuchong356 <atgugui@163.com>
2022-02-11 00:31:33 +0800

    create DJJJ

---
+++ src/main/java/com/atguigu/wugong/Dugujiujian.java
@@ -0,0 +1,17 @@
+package com.atguigu.wugong;
+
+/**
+ * title:
+ * Lenovo On 2022/2/11
+ * description
+ */
+public class Dugujiujian {
+	public static void main(String[] args) {
+		System.out.println("独孤九剑");
+		System.out.println("独孤九剑");
+		System.out.println("独孤九剑");
+		System.out.println("独孤九剑");
+		System.out.println("独孤九剑");
+		System.out.println("独孤九剑");
+	}
+}
